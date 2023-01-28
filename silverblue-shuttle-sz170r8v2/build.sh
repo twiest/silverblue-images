8bf721a170adc4759929441821afa83ca70f278c
Thomas Wiest <th.wiest@celonis.com>
2023-01-28 16:38:35 -0500

    Add silverblue-shuttle-sz170r8v2

---
+++ silverblue-shuttle-sz170r8v2/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+
+err_report() {
+    echo
+    echo
+    echo -e "${RED}Error $1 occured on line $2${NO_COLOR}"
+    echo
+    exit $1
+}
+
+trap 'err_report $? $LINENO' ERR
+
+cd $(dirname $0)
+
+IMGNAME=$(basename $PWD)
+DATESTAMP=$(date +%Y-%m-%d)
+
+time podman build $@ . -t "${IMGNAME}:${DATESTAMP}" -t "$IMGNAME:latest"
