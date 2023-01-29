b7c0d49d00371dec0ecfb18ace57fecb25aec91d
Thomas Wiest <th.wiest@celonis.com>
2023-01-29 16:09:24 -0500

    Add common packages to base image.

--- silverblue-shuttle-sz170r8v2/install-nvidia-akmod.sh
+++ silverblue-shuttle-sz170r8v2/install-nvidia-akmod.sh
@@ -3,6 +3,7 @@
 mkdir -p /var/log/akmods /var/tmp /run/akmods
 chmod --reference=/tmp /var/tmp
 
+# TODO: Remove the following line once this bug is fixed: https://github.com/coreos/rpm-ostree/issues/4201
 [ -f /usr/bin/ld ] || ln -s /usr/bin/ld.bfd /usr/bin/ld
 
 /usr/sbin/akmods --force --kernels "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
