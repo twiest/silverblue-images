2df8e24fe6818b20c3709dca345ba999499f589e
Thomas Wiest <twg-github@wiestfamily.org>
2024-03-31 12:07:54 -0400

    Change config.d.sh to be able to use multiple directories.

--- silverblue-base/files/config.d/config.d.sh
+++ silverblue-base/files/config.d/config.d.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-configd_dir=/etc/config.d
+configd_dirs="/etc/config.d /usr/local/etc/config.d /usr/local/etc/$(hostname)/config.d"
 
-mkdir -p "$configd_dir"
+for configd_dir in $configd_dirs; do
+  if [ ! -d "$configd_dir" ]; then
+    echo "Skipping $configd_dir"
+    continue
+  fi
 
-echo "Running config.d scripts:"
-for script in $(ls "$configd_dir"); do
-  echo "    Running [$script]"
+  echo "Running $configd_dir scripts:"
+  for script in $(ls "$configd_dir"); do
+    echo "    Running [$script]"
 
-  # Run the script and pre-pend padding so that it looks good in journald
-  bash "${configd_dir}/${script}" | sed 's/^/        /'
+    # Run the script and pre-pend padding so that it looks good in journald
+    bash "${configd_dir}/${script}" | sed 's/^/        /'
+  done
 done
