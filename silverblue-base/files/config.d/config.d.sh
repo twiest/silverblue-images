82ada99f95b1e4e96bc310f2500a1e195e15a2e7
Thomas Wiest <twg-github@wiestfamily.org>
2024-03-31 12:20:28 -0400

    Fix bug in config.d.sh where it tries to run dirs and non-executable files.

--- silverblue-base/files/config.d/config.d.sh
+++ silverblue-base/files/config.d/config.d.sh
@@ -4,15 +4,25 @@ configd_dirs="/etc/config.d /usr/local/etc/config.d /usr/local/etc/$(hostname)/c
 
 for configd_dir in $configd_dirs; do
   if [ ! -d "$configd_dir" ]; then
-    echo "Skipping $configd_dir"
+    echo "Skipping [$configd_dir]... directory doesn't exist."
     continue
   fi
 
   echo "Running $configd_dir scripts:"
-  for script in $(ls "$configd_dir"); do
+  for curfile in $(ls "$configd_dir"); do
+    script="${configd_dir}/${curfile}"
+    if [ -d "$script" ]; then
+      continue
+    fi
+
+    if [ ! -x "$script" ]; then
+      echo "    Skipping [$script]... not executable"
+      continue
+    fi
+
     echo "    Running [$script]"
 
     # Run the script and pre-pend padding so that it looks good in journald
-    bash "${configd_dir}/${script}" | sed 's/^/        /'
+    bash "${script}" | sed 's/^/        /'
   done
 done
