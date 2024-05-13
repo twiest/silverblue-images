da92c83f643331fff081bec2988458670f49236a
Thomas Wiest <twg-github@wiestfamily.org>
2024-05-13 12:41:52 -0400

    silverblue-base: change config.d to look in /usr/local/etc/config.d/all

--- silverblue-base/files/config.d/config.d.sh
+++ silverblue-base/files/config.d/config.d.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-configd_dirs="/etc/config.d /usr/local/etc/config.d /usr/local/etc/config.d/$(hostname)"
+configd_dirs="/etc/config.d /usr/local/etc/config.d/all /usr/local/etc/config.d/$(hostname)"
 
 for configd_dir in $configd_dirs; do
   if [ ! -d "$configd_dir" ]; then
