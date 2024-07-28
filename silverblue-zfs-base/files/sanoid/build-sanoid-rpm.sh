c1db66068dd29498c1fe2f06c2f3731d6ff68d08
Thomas Wiest <twg-github@wiestfamily.org>
2024-07-27 20:09:49 -0400

    Fix sanoid build script. Update sanoid rpm to fc40.

--- silverblue-zfs-base/files/sanoid/build-sanoid-rpm.sh
+++ silverblue-zfs-base/files/sanoid/build-sanoid-rpm.sh
@@ -3,6 +3,7 @@
 # Change the script dir
 cd $(dirname -- ${BASH_SOURCE[0]})
 
-image=$(awk '/FROM/ { print $2 }' ../../Containerfile)
+image=$(awk '/FROM.*quay.io/ { print $2 }' ../../Containerfile)
+#FROM quay.io/fedora/fedora-silverblue:40
 
 podman run -it --rm -v $PWD:/build:z "$image" /build/in-ctr-build.sh
