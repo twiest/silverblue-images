b390f8b1d694166d80aeeaebb2078f3a81e7acac
Thomas Wiest <twg-github@wiestfamily.org>
2024-07-28 00:03:44 -0400

    Change silverblue-shared-builder-python-validity to have the source rpms in tree.

---
+++ silverblue-shared-builder-python-validity/files/download-srpms.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://download.copr.fedorainfracloud.org/results/tigro/python-validity/fedora-38-x86_64/05739175-python-validity/python-validity-0.14-2.fc38.src.rpm
+
+wget https://download.copr.fedorainfracloud.org/results/tigro/python-validity/fedora-38-x86_64/05739127-fprintd-clients/fprintd-clients-1.94.2-2.fc38.src.rpm
+
+wget https://download.copr.fedorainfracloud.org/results/tigro/python-validity/fedora-38-x86_64/05739185-open-fprintd/open-fprintd-0.6-7.fc38.src.rpm
+
