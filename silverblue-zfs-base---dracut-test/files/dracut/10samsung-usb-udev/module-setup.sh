74032a5e6e909ad2ce87147e65495198f4f87ea0
Thomas Wiest <twg-github@wiestfamily.org>
2025-06-24 09:05:16 -0400

    silverblue-zfs-base---dracut-test: remove since it is no longer needed

--- silverblue-zfs-base---dracut-test/files/dracut/10samsung-usb-udev/module-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# called by dracut
-install() {
-    inst_rules "$moddir/10-samsung-usb.rules"
-
-    prepare_udev_rules 10-samsung-usb.rules
-}
--- silverblue-zfs-base---dracut-test/files/dracut/20silicon-power-udev/module-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# called by dracut
-install() {
-    inst_rules "$moddir/20-silicon-power.rules"
-
-    prepare_udev_rules 20-silicon-power.rules
-}
