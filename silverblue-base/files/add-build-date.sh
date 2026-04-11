e3cf058b319f835419ed48a6f36b079dd4475f1d
Thomas Wiest <twg-github@wiestfamily.org>
2026-04-11 15:23:13 -0400

    silverblue-base: fix build date script given that worldtimeapi.org went offline

--- silverblue-base/files/add-build-date.sh
+++ silverblue-base/files/add-build-date.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-date_info="/tmp/new_york_date_info.json"
+date_info="/tmp/gmc_date_info"
 cloud_dir="/etc/cloud"
 build_info_file="$cloud_dir/build.info"
-worldtimeapi="worldtimeapi.org"
+datetime_url="https://google.com"
 
 # Make sure we capture the date info. worldtimeapi.org sometimes rejects api calls
-while ! curl "http://${worldtimeapi}/api/timezone/America/New_York" > $date_info 2> /dev/null ; do
-  echo -n "ERROR: curl failed calling  ${worldtimeapi} at "
+while ! curl -sI "$datetime_url" | awk '/^date:/ { $1=""; print $0 }' > $date_info 2> /dev/null ; do
+  echo -n "ERROR: curl failed calling  ${datetime_url} at "
   date
   sleep 1
 done
 
-build_date=$(jq -r .datetime $date_info | cut -d'T' -f1 | tr -d '-')
+build_date=$(date -d "$(cat $date_info)" +%Y-%m-%d)
 
 echo "Success! Setting build date to [$build_date]"
 
