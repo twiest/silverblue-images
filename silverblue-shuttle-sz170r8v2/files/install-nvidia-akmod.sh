9a258e231c397aa58e75e1672b0b70d4ed06bece
Thomas Wiest <twg-github@wiestfamily.org>
2026-04-12 23:03:43 -0400

    silverblue-shuttle-sz170r8v2: fix nvidia akmod build script to properly error when build fails. Also fix build errors.

--- silverblue-shuttle-sz170r8v2/files/install-nvidia-akmod.sh
+++ silverblue-shuttle-sz170r8v2/files/install-nvidia-akmod.sh
@@ -3,12 +3,26 @@
 set -euo pipefail
 
 mkdir -p /var/log/akmods /var/tmp /run/akmods
-chmod --reference=/tmp /var/tmp
+# Set the perms so akmods can build
+chmod 777 /tmp /var/tmp
 
 # TODO: Remove the following line once this bug is fixed: https://github.com/coreos/rpm-ostree/issues/4201
 [ -f /usr/bin/ld ] || ln -s /usr/bin/ld.bfd /usr/bin/ld
 
-/usr/sbin/akmods --force --kernels "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+kernel_version="$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+/usr/sbin/akmods --force --kernels "$kernel_version"
+
+# Set the perms back
+chmod 755 /tmp /var/tmp
+
+if [ -d "/lib/modules/${kernel_version}/extra/nvidia" ]; then
+  echo
+  echo "nvidia akmod build succeeded!"
+  echo
+else
+  echo "ERROR: nvidia akmod failed to build!"
+  exit 10
+fi
 
 [ -h /usr/bin/ld ] && rm /usr/bin/ld
 rm -rf /var/log/akmods/ /var/tmp /run/akmods/
